a29ad4da6bccc6e347f91f3e03e975f144b6ab55
akashmelkeri-incentius <akash.melkeri@incentius.com>
2023-08-17 19:54:23 +0530

    drawer

---
+++ src/components/Drawer.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="h-full flex flex-col flex-nowrap">
+    <div class="flex items-center gap-4 p-4 flex-shrink-0">
+      <div class="text-3xl poppins font-bold ">
+        <span class="bg-gradient-to-r from-purple-600 via-blue-600 to-blue-500 inline-block text-transparent bg-clip-text">Free</span>
+        <span class="text-gray-500">mium</span>
+      </div>
+    </div>
+    <div class="flex-grow ">
+      <div class="h-full overflow-y-auto">
+        <q-list>
+          <template v-for="category in tools_by_category">
+            <q-item-label header>
+              {{ category.label }}
+            </q-item-label>
+            <q-item @click="$router.push(tool.path)" v-for="tool in category.tools" clickable v-ripple active-class="text-blue-100 bg-gradient-to-r from-purple-600 via-blue-600 to-blue-500" class="text-gray-500 " :active="$route.path == tool.path">
+              <q-item-section avatar dense>
+                <q-icon :name="tool.icon"></q-icon>
+              </q-item-section>
+              <q-item-section label class="">
+                {{ tool.title }}
+              </q-item-section>
+            </q-item>
+          </template>
+        </q-list>
+      </div>
+    </div>
+    <div class="flex-shrink-0 text-center p-4  w-full">
+      <span class="text-gray-500 font-bold">Made with <i class="q-icon text-red notranslate material-icons" aria-hidden="true" role="presentation"
+        style="font-size: 18px;">favorite</i> by <span class="bg-gradient-to-r from-purple-600 via-blue-600 to-blue-500 inline-block text-transparent bg-clip-text">Incentius</span></span>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'DrawerComponent',
+  props: {},
+  setup() {
+    return {
+      tools_by_category:[
+        {
+          label:'Text',
+          tools:[{
+          path: '/',
+          title: 'Convert text style',
+          icon: 'home',
+        },
+        {
+          path: '/base64',
+          title: 'Base64 Converter',
+          icon: 'code',
+        },
+        {
+          path: '/urlencodedecode',
+          title: 'URL Enode Decode',
+          icon: 'link',
+        },
+        {
+          path: '/qrcode',
+          title: 'QR-Code Generator',
+          icon: 'qr_code_scanner',
+        }],
+        },
+        {
+          label:'Image',
+          tools:[{
+            path: '/base64Image',
+            title: 'Base64 Image Converter',
+            icon: 'image',
+        }],
+        },
+        {
+          label:'Data',
+          tools:[{
+            path: '/jsonformatter',
+            title: 'JSON Formatter',
+            icon: 'data_object',
+          },{
+            path: '/jsoncsv',
+            title: 'JSON TO CSV',
+            icon: 'description',
+          },{
+            path: '/sqlformatter',
+            title: 'SQL Formatter',
+            icon: 'article'
+          },{
+            path: '/exceltojson',
+            title: 'Excel To JSON',
+            icon: 'source',
+          }],
+        },
+      ],
+    }
+  }
+})
+</script>
+<style scoped>
+
+</style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -9,287 +9,8 @@
     <!--        <div>Quasar v{{ $q.version }}</div>-->
     <!--      </q-toolbar>-->
     <!--    </q-header>-->
-    <q-drawer v-if="collapse"
-              :width='65'
-              v-model="leftDrawerOpen"
-              show-if-above
-              bordered
-              class="flex flex-col bg-brown-1"
-    >
-      <div>
-        <div class="">
-          <img class="q-mx-auto my-4 ml-1" src="../assets/freemiumlogo.png" style="width: 90%;">
-        </div>
-        <q-separator></q-separator>
-        <div v-if="!$q.screen.lt.sm" class="my-2 ml-1">
-          <q-btn icon="reorder" flat @click="collapse=!collapse;">
-            <q-tooltip size="10px">Open the sidebar</q-tooltip>
-          </q-btn>
-        </div>
-
-
-        <div :key="index" v-for="(link,index) in getSearchData" class="px-1">
-          <a @click="$route.path==link.path ? '' : $router.push(link.path)"
-             :class="$route.path==link.path ? 'bg-blue-500 text-white rounded-xl border-gray-700' : 'text-black hover:bg-bl-700 hover:text-white'"
-             class="cursor-pointer group flex items-center px-4 py-2 text-sm font-medium"
-             aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
-            <q-icon size="sm" class=" align-center" :name="link.icon" alt=""/>
-            <q-tooltip>{{ link.title }}</q-tooltip>
-
-          </a>
-          <!-- <q-item
-                clickable
-                tag="a"
-                target="_blank"
-                @click="$router.push('/')"
-              >
-                <q-item-section
-                  v-if="test.icon"
-                  avatar
-                >
-                  <q-icon  name="home"/>
-                </q-item-section>
-
-
-              </q-item> -->
-
-          <!-- For Json formatter -->
-          <!-- <q-item
-              clickable
-              tag="a"
-              target="_blank"
-              @click="$router.push('/jsonformatter')"
-            >
-              <q-item-section
-                v-if="test.icon"
-                avatar
-              >
-                <q-icon name="data_object"/>
-              </q-item-section>
-
-            </q-item> -->
-
-          <!-- For JSON To CSV-->
-          <!-- <q-item
-            clickable
-            tag="a"
-            target="_blank"
-            @click="$router.push('/jsoncsv')"
-          >
-            <q-item-section
-              v-if="test.icon"
-              avatar
-            >
-              <q-icon name="description"/>
-            </q-item-section>
-          </q-item> -->
-
-          <!-- For SQL formatter -->
-          <!-- <q-item
-            clickable
-            tag="a"
-            target="_blank"
-            @click="$router.push('/sqlformatter')"
-          >
-            <q-item-section
-              v-if="test.icon"
-              avatar
-            >
-              <q-icon name="article"/>
-            </q-item-section>
-          </q-item> -->
-          <!-- For Base 64 -->
-          <!-- <q-item
-            clickable
-            tag="a"
-            target="_blank"
-            @click="$router.push('/base64')"
-          >
-            <q-item-section
-              v-if="test.icon"
-              avatar
-            >
-              <q-icon  name="code"/>
-            </q-item-section>
-          </q-item> -->
-          <!-- For Json Formatter -->
-          <!-- <q-item
-            clickable
-            tag="a"
-            target="_blank"
-            @click="$router.push('/base64Image')"
-          >
-            <q-item-section
-              v-if="test.icon"
-              avatar
-            >
-              <q-icon  name="image"/>
-            </q-item-section>
-          </q-item> -->
-
-
-          <!-- For SQL formatter -->
-          <!-- <q-item
-            clickable
-            tag="a"
-            target="_blank"
-            @click="$router.push('/urlencodedecode')"
-          >
-            <q-item-section
-              v-if="test.icon"
-              avatar
-            >
-              <q-icon  name="link"/>
-            </q-item-section>
-          </q-item> -->
-
-          <!-- For PDF Generator-->
-          <!-- <q-item
-            clickable
-            tag="a"
-            target="_blank"
-            @click="$router.push('/exceltojson')"
-          >
-            <q-item-section
-              v-if="test.icon"
-              avatar
-            >
-              <q-icon name="source"/>
-            </q-item-section>
-          </q-item> -->
-
-          <!-- For QR-Code Generator-->
-          <!-- <q-item
-            clickable
-            tag="a"
-            target="_blank"
-            @click="$router.push('/qrcode')"
-          >
-            <q-item-section
-              v-if="test.icon"
-              avatar
-            >
-              <q-icon name="qr_code_scanner"/>
-            </q-item-section>
-          </q-item> -->
-
-          <!-- For PDF Generator-->
-          <!-- <q-item
-            clickable
-            tag="a"
-            target="_blank"
-            @click="$router.push('/texttopdf')"
-          >
-            <q-item-section
-              v-if="test.icon"
-              avatar
-            >
-              <q-icon name="picture_as_pdf"/>
-            </q-item-section>
-          </q-item> -->
-        </div>
-        <!-- <div  v-if="!$q.screen.lt.sm" class="mt-10">
-          <q-btn icon="reorder" flat @click="collapse=!collapse;"><q-tooltip size="10px">Open the sidebar</q-tooltip></q-btn>
-        </div> -->
-      </div>
-    </q-drawer>
-    <q-drawer v-else
-              v-model="leftDrawerOpen"
-              show-if-above
-              bordered
-              class="flex flex-col bg-brown-1"
-    >
-      <q-list>
-        <div class="overflow-hidden mx-3">
-          <div class="row">
-            <img class="q-mx-auto my-1" src="../assets/freemiumlogo.png" height="80" style=" width: 100%;">
-          </div>
-          <q-separator></q-separator>
-          <!-- <div class="row">
-            <div class="col-9 my-2 ml-2">
-              <q-input class="float-right q-my-sm q-mr-sm" outlined color="gray" dense debounce="300" v-model="search_data"
-                  placeholder="search">
-                <template v-slot:append>
-                  <q-icon color="gray-600" name="search"/>
-                </template>
-              </q-input>
-            </div>
-            <div class="col-2 mt-4 ml-2">
-              <q-btn classs="float-right" icon="reorder" flat @click="collapse=!collapse;" ><q-tooltip>Close the sidebar</q-tooltip></q-btn>
-            </div>
-          </div> -->
-
-
-          <div class="mt-2">
-            <div
-              v-for="link in getSearchData"
-              :key="link.title"
-              v-bind="link"
-            >
-              <a @click="$route.path==link.path ? '' : $router.push(link.path)"
-                 :class="$route.path==link.path ? 'bg-blue-500 text-white rounded-xl border-gray-700' : 'text-black hover:bg-bl-700 hover:text-white'"
-                 class="cursor-pointer group flex items-center px-2 py-2 text-sm font-medium"
-                 aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
-                <q-icon size="xs" class="mr-3 h-6" :name="link.icon" alt=""/>
-                {{ link.title }}
-
-              </a>
-            </div>
-          </div>
-
-          <!-- <div class="absolute-bottom" v-if="!collapse">
-            <q-separator></q-separator>
-            <q-item>
-              <q-item-section avatar>
-                <q-avatar>
-                  <img src="https://cdn.quasar.dev/img/avatar6.jpg">
-                </q-avatar>
-              </q-item-section>
-
-              <q-item-section>
-                <q-item-label>Winfield Stapforth</q-item-label>
-              </q-item-section>
-
-              <q-item-section side>
-                <q-btn icon="logout" flat dense color="grey"/>
-              </q-item-section>
-            </q-item>
-          </div> -->
-          <!-- <q-item class="mt-12"
-              clickable
-              @click="collapse=!collapse;"
-            >
-              <q-item-section
-
-                avatar
-              >
-                <q-icon name="reorder"/>
-              </q-item-section>
-            <q-item-section>
-                <q-item-label class="">Close sidebar</q-item-label>
-              </q-item-section>
-            </q-item> -->
-          <!-- <div class="mt-4">
-              <q-btn class="text-capitalize" icon="reorder" flat @click="collapse=!collapse;" ><q-tooltip>Close the sidebar</q-tooltip>Close sidebar</q-btn>
-            </div> -->
-        </div>
-
-      </q-list>
-      <div class="footer flex justify-center q-mt-md text-center text-body2 text-secondary mb-2">
-          <span> Made with
-            <span>
-              <i class="q-icon text-red notranslate material-icons" aria-hidden="true" role="presentation"
-                 style="font-size: 18px;">favorite</i> by
-            </span>
-              <a href="https://incentius.com" class="text-blue text-bold">Incentius</a>
-          </span>
-      </div>
-
-
-      <!-- <div  v-if="!$q.screen.lt.sm" class="mt-2 flex items-center justify-center">
-        <q-btn icon="reorder" flat @click="collapse=!collapse;" ><q-tooltip>Close the sidebar</q-tooltip></q-btn>
-        </div> -->
-
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered >
+      <Drawer />
     </q-drawer>
     <q-page-container class="backgroundcolor">
       <router-view/>
@@ -301,6 +22,7 @@
 <script>
 import EssentialLink from 'components/EssentialLink.vue'
 // import Sidebarcollapse from 'components/Sidebarcollapse.vue'
+import Drawer from 'src/components/Drawer.vue'
 
 const linksList = [
   {
@@ -363,12 +85,14 @@ export default defineComponent({
 
   components: {
     EssentialLink,
+    Drawer,
   },
 
   setup() {
     const leftDrawerOpen = ref(false)
 
     return {
+      leftDrawerOpen,
       collapse: ref(false),
       essentialLinks: ref(linksList),
       search_data: ref('')
